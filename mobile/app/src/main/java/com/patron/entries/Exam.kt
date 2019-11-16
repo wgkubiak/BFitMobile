daaed7227dd94f19ca15a83d5ab5b80ef4cd375e
wgkubiak <wgkubiak@gmail.com>
2019-11-16 23:59:25 +0100

    Project language change

---
+++ mobile/app/src/main/java/com/patron/entries/Exam.kt
@@ -0,0 +1,8 @@
+package com.patron.entries
+
+class Exam constructor(
+    val examProtege: String,
+    val examWeight: String,
+    val examGlucose: String,
+    val examPressure: String
+) {}
---
+++ mobile/app/src/main/java/com/patron/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.patron.utils
+
+const val PATRONS_URL = "https://patronapi.herokuapp.com/patrons/"
+const val PROTEGES_URL = "https://patronapi.herokuapp.com/proteges/"
+const val EXAMS_URL = "https://patronapi.herokuapp.com/exams/"
+const val EXAMS_BYID_URL = "https://patronapi.herokuapp.com/exams/id/"
