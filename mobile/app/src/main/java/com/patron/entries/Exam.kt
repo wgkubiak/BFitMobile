107599473b05b425f70f7e1f8fdb966df907b202
wgkubiak <wgkubiak@gmail.com>
2019-11-20 21:41:26 +0100

    Layout of Login Screen

--- mobile/app/src/main/java/com/patron/entries/Exam.kt
+++
@@ -1,8 +0,0 @@
-package com.patron.entries
-
-class Exam constructor(
-    val examProtege: String,
-    val examWeight: String,
-    val examGlucose: String,
-    val examPressure: String
-) {}
--- mobile/app/src/main/java/com/patron/utils/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.patron.utils
-
-const val PATRONS_URL = "https://patronapi.herokuapp.com/patrons/"
-const val PROTEGES_URL = "https://patronapi.herokuapp.com/proteges/"
-const val EXAMS_URL = "https://patronapi.herokuapp.com/exams/"
-const val EXAMS_BYID_URL = "https://patronapi.herokuapp.com/exams/id/"
